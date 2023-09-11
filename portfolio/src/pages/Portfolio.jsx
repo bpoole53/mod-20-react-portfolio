24866e69366af8497d3526aab086073bb889d4fd
bpoole53 <bpoole53@gmail.com>
2023-09-11 02:19:47 -0500

    added to the portfolio page

--- portfolio/src/pages/Portfolio.jsx
+++ portfolio/src/pages/Portfolio.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Project } from '../components'
 
 export default function Portfolio(){
 
 	return(
 	<>
-		
+        <Project />
 	</>	
 	);
 }
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/Footer.jsx', './src/components/Header.jsx', './src/components/Navigation.jsx', './src/pages/About.jsx', './src/pages/Contact.jsx'],
+  content: ['./src/components/Footer.jsx', './src/components/Header.jsx', './src/components/Navigation.jsx', './src/pages/About.jsx', './src/pages/Contact.jsx', './src/pages/Portfolio.jsx', './src/pages/Resume.jsx', './src/components/Project.jsx'],
   theme: {
     extend: {},
   },
