86575ae1caf008fb658e6d22c0ccc889fe171528
bpoole53 <bpoole53@gmail.com>
2023-09-11 04:17:53 -0500

    removed some more errors

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -9,23 +9,23 @@ export default function Resume(){
             <h2 className="text-2xl font-bold">Download my <a className="text-white" href="/resume">Resume</a></h2>            
             <h2 className="font-bold">Front-end Proficiencies</h2>    
             <ul className="list-disc">
-                <li><a>HTML</a></li>
-                <li><a>CSS</a></li>
-                <li><a>JavaScript</a></li>
-                <li><a>JQuery</a></li>
-                <li><a>Responsive Design</a></li>
-                <li><a>React</a></li>
-                <li><a>Bootstrap</a></li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>JQuery</li>
+                <li>Responsive Design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
             </ul>
             <h2 className="font-bold">Back-end Proficiencies</h2>    
             <ul className="list-disc">
-                <li><a>API's</a></li>
-                <li><a>Node</a></li>
-                <li><a>Express</a></li>
-                <li><a>MySQL Sequelize</a></li>
-                <li><a>MongoDB, Mongoose</a></li>
-                <li><a>REST</a></li>
-                <li><a>GraphQL</a></li>
+                <li>API's</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
             </ul>
         </div>        
 	</>	
