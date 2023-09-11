966132d23ff88c87b75f72946b039e2ee3369ccd
bpoole53 <bpoole53@gmail.com>
2023-09-11 01:25:01 -0500

    added a contact me page

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 export default function Contact(){
 
 	return(
-	<>
-		
+	<>    
+        <div class="about-page">
+            <h1 className="text-5xl font-bold">Contact Me</h1>  
+            <input class="contact-input" type="text" placeholder="Name" className="input input-bordered w-full max-w-md text-white" />
+            <input class="contact-input" type="text" placeholder="Email Address" className="input input-bordered w-full max-w-md text-white" />
+            <textarea class="contact-input" type="text" placeholder="Message" className="input input-bordered w-full max-w-md h-36 text-white pb-4"></textarea>
+            <a className="btn searchBtn" type="submit">Submit</a>
+        </div>
 	</>	
 	);
 }
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/Footer.jsx', './src/components/Header.jsx', './src/components/Navigation.jsx', './src/pages/About.jsx'],
+  content: ['./src/components/Footer.jsx', './src/components/Header.jsx', './src/components/Navigation.jsx', './src/pages/About.jsx', './src/pages/Contact.jsx'],
   theme: {
     extend: {},
   },
